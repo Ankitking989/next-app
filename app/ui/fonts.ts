bd04442af937171ed51cf84eaf6bdea02180b574
Ankit King <ankitking793@gmail.com>
2026-04-22 16:51:34 +0530

    update

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,12 @@
 import { Inter, Lusitana, Roboto, Open_Sans, Poppins, } from 'next/font/google';
 
-export const inter = Inter({subset: ['latin']});
+export const inter = Inter({subsets: ['latin']});
 
-export const lusitana = Lusitana({subset: ['latin'], weight: ['400', '700']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400', '700']});
 
-export const poppins = Poppins({subset: ['Latin'], weight: ['400']});
+export const poppins = Poppins({subsets: ['Latin'], weight: ['400']});
 
-export const open_sans = Open_Sans({subset: ['latin'], weight: ['400']});
+export const open_sans = Open_Sans({subsets: ['latin'], weight: ['400']});
 
 
 
